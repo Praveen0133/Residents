f4866beb6e0eccde6d99e79c62d68679a84925fd
Praveen0133 <praveenkumarsingh034@gmail.com>
2025-06-06 13:02:36 +0530

    mongoURL update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 
 const connectDb=async ()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/Residency')
+        await mongoose.connect('mongodb+srv://real_estate_app:Praveen%40034@cluster0.2llqy.mongodb.net/Residency?retryWrites=true&w=majority&appName=Cluster0')
         console.log("db connection succefully");
     } catch (error) {
         console.log("mongoose connection fail")
